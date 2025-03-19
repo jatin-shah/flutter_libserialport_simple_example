d5d7ccfe7b93066f626c38677911077620d0b68c
Jay Shah <jay.shah@tombot.com>
2025-03-19 10:03:42 -0700

    Updated comms baud rate to 115200

--- lib/main.dart
+++ lib/main.dart
@@ -50,12 +50,12 @@ class _MyHomePageState extends State<MyHomePage> {
     for (final name in SerialPort.availablePorts) {
       final sp = SerialPort(name);
       if (kDebugMode) {
-        print('${++i}) $name');
-        print('\tDescription: ${cp949.decodeString(sp.description ?? '')}');
-        print('\tManufacturer: ${sp.manufacturer}');
-        print('\tSerial Number: ${sp.serialNumber}');
-        print('\tProduct ID: 0x${sp.productId?.toRadixString(16) ?? 00}');
-        print('\tVendor ID: 0x${sp.vendorId?.toRadixString(16) ?? 00}');
+        debugPrint('${++i}) $name');
+        debugPrint('\tDescription: ${cp949.decodeString(sp.description ?? '')}');
+        debugPrint('\tManufacturer: ${sp.manufacturer}');
+        debugPrint('\tSerial Number: ${sp.serialNumber}');
+        debugPrint('\tProduct ID: 0x${sp.productId?.toRadixString(16) ?? 00}');
+        debugPrint('\tVendor ID: 0x${sp.vendorId?.toRadixString(16) ?? 00}');
       }
       portList.add(sp);
     }
@@ -139,12 +139,12 @@ class _MyHomePageState extends State<MyHomePage> {
                                             curve:Curves.easeOut,
                                             duration: const Duration(milliseconds: 100),);
                       }
-                      /*
-                      OUTPUT for raw bytes 
-                      return Text(receiveDataList[index].toString()); */
+                      /* OUTPUT for raw bytes */
+                      // return Text(receiveDataList[index].toString()); 
+                      return Text(receiveDataList[index].map((byte)=> byte.toRadixString(16).toUpperCase().padLeft(2, '0')).join(' ')); 
                       
-                      /* output for string */
-                      return Text(String.fromCharCodes(receiveDataList[index]).replaceAll('\n', '')); 
+                      /* output for string 
+                      return Text(String.fromCharCodes(receiveDataList[index]).replaceAll('\n', ''));  */
                     },),
               ),
             ),
@@ -168,7 +168,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 ),
                 Flexible(
                   child: TextButton.icon(
-                    onPressed: sendSerialOutput,
+                    onPressed: sendTestData, //sendSerialOutput,
                     icon: const Icon(Icons.send),
                     label: const Text("Send"),
                   ),
@@ -193,7 +193,7 @@ class _MyHomePageState extends State<MyHomePage> {
         SerialPortConfig config = _serialPort!.config;
         // https://www.sigrok.org/api/libserialport/0.1.1/a00007.html#gab14927cf0efee73b59d04a572b688fa0
         // https://www.sigrok.org/api/libserialport/0.1.1/a00004_source.html
-        config.baudRate = 57600;
+        config.baudRate = 115200;
         config.parity = 0;
         config.bits = 8;
         config.cts = 0;
@@ -243,16 +243,33 @@ class _MyHomePageState extends State<MyHomePage> {
     }
   }
 
+  void sendTestData() {
+    Uint8List data = Uint8List.fromList([0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1A, 0x1B, 0x1C, 0x1D, 0x1E, 0x1F, 0x20, 0x21, 0x22, 0x23, 0x24, 0x25, 0x26, 0x27, 0x28, 0x29, 0x2A, 0x2B, 0x2C, 0x2D, 0x2E, 0x2F, 0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39, 0x3A, 0x3B, 0x3C, 0x3D, 0x3E, 0x3F, 0x40, 0x41, 0x42, 0x43, 0x44, 0x45, 0x46, 0x47]);
+    SerialPort port = _serialPort!;
+    if (port != null && port.isOpen) {
+      var writeLen = port.write(data);
+      debugPrint('Wrote: $data, Bytes sent: $writeLen');
+      if (writeLen == data.length) {
+        setState(() {
+          textInputCtrl.text = '';
+        });
+      }
+    }
+  }
+
   void changeScrollSetting(bool? newValue) {
     _scrollEnable = newValue as bool;
   }
 
   void clearWindow() {
-    receiveDataList.clear();
+    setState(() {
+      receiveDataList.clear();
+    });
   }
 
   void processCmdCompletion(text) {
     // debugPrint(text);
-    sendSerialOutput();
+    // sendSerialOutput();
+    sendTestData();
   }
 }
